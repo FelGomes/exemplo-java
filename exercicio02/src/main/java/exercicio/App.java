704683fcd86b05e4f80127b2ee4b8fec6d161755
FelGomes <felipeferreiraag0@gmail.com>
2024-10-04 20:35:54 -0300

    coloquei meu nome

--- exercicio02/src/main/java/exercicio/App.java
+++ exercicio02/src/main/java/exercicio/App.java
@@ -5,6 +5,7 @@ package exercicio;
 public class App {
     public static void main(String[] args) {
         System.out.println("oiiiii galera");
+        System.out.println("EU SOU O FELIPE! ");
     }
     
     
